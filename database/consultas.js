efd2898c1786905af380843b963312f1c7713c15
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-09-08 12:43:41 -0300

    Add files via upload

---
+++ database/consultas.js
@@ -0,0 +1,39 @@
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+// Base de datos y tabla que debe ser creada en PostgreSQL
+// CREATE DATABASE likeme;
+// CREATE TABLE posts (id SERIAL, titulo VARCHAR(25), img VARCHAR(1000),descripcion VARCHAR(255), likes INT);
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'lore',
+    password: '123456',
+    database: 'likeme',
+    port: 5432,
+    allowExitOnIdle: true,
+   });
+
+
+const verPosts = async () => {
+  const { rows } = await pool.query('SELECT * FROM posts');
+
+  return rows;
+};
+
+const agregarPost = async (post) => {
+  const { titulo, img, descripcion } = post
+
+  if (!titulo?.trim() || !img?.trim() || !descripcion?.trim()) {
+    throw { code: '400' };
+  }
+
+  const consulta = 'INSERT INTO posts values (DEFAULT, $1, $2, $3, $4) RETURNING *';
+  const values = [titulo, img, descripcion, 0];
+  const result = await pool.query(consulta, values);
+
+  return result.rows[0];
+};
+
+export { verPosts, agregarPost };
